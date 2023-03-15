5c944c1d779da50dc794469b264221505b27842f
ylee <ylee@student.42seoul.kr>
2023-03-15 13:20:40 +0900

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by 이유리 on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
