b0763f19469533f4ac5153566b3815799f81d1c8
ylee <ylee@student.42seoul.kr>
2023-03-21 18:22:01 +0900

    finish tutorial 2-8 set preview dynamically for many different devices
    https://developer.apple.com/tutorials/swiftui/building-lists-and-navigation#Generate-Previews-Dynamically

--- testApp/Views/LandmarkList.swift
+++ testApp/Views/LandmarkList.swift
@@ -25,6 +25,17 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(
+            [
+                "iPhone SE (2nd generation)",
+                "iPhone XS Max",
+                "iPad mini (6th generation)"
+            ],
+            id: \.self
+        ) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
     }
 }
